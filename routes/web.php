eb3b4639faa55c8a868362f2d226f7aae42d95e3
190113013 <78861910+190113013@users.noreply.github.com>
2021-02-27 15:25:42 +0600

    lab6

--- routes/web.php
+++ routes/web.php
@@ -46,5 +46,75 @@ Route::get('/lab5/{name}', 'App\Http\Controllers\StudentController@lab5');
 Route::get('/sdob/{dob}', 'App\Http\Controllers\StudentController@rdob');
 Route::get('/sage/{age}', 'App\Http\Controllers\StudentController@rage');
 
+Route::get('/insert', function(){
+	DB::insert('insert into student(ID, Name, Date_of_birth, GPA, Advisor)
+				values(?, ?, ?, ?, ?)', ['1913', 'Yerzhan Sandubayev',
+										'2001-07-18', '3.5', 'Yerbol']);
+});
+
+Route::get('/select', function(){
+	$results=DB::select('select * from student where id=?', [190113013]);
+	foreach($results as $posts) {
+		echo "ID is ". $posts->ID;
+		echo "<br>";
+		echo "Name is ". $posts->Name;
+		echo "<br>";
+		echo "Date of birth is ". $posts->Date_of_birth;
+		echo "<br>";
+		echo "GPA is ". $posts->GPA;
+		echo "<br>";
+		echo "Advisor is ". $posts->Advisor;
+	}
+});
+
+Route::get('/update', function(){
+	$up=DB::update('update student set GPA = "2.9" where id=?', [190113013]);
+	return $up;
+});
+
+Route::get('/delete', function(){
+	$del=DB::delete('delete from student where id=?', [1913]);
+	return $del;
+});
+
+use App\Models\Student;
+Route::get('/read', function(){
+	$sts=Student::all();
+	foreach($sts as $st) {
+		echo "ID is ". $st->ID;
+		echo "<br>";
+		echo "Name is ". $st->Name;
+		echo "<br>";
+		echo "Date of birth is ". $st->Date_of_birth;
+		echo "<br>";
+		echo "GPA is ". $st->GPA;
+		echo "<br>";
+		echo "Advisor is ". $st->Advisor;
+	}
+});
+
+Route::get('/insert1', function(){
+	$sts = new Student;
+	$sts -> ID = '19030303';
+	$sts -> Name = 'John Stones';
+	$sts -> Date_of_birth = '2001-09-09';
+	$sts -> GPA = '2.5';
+	$sts -> Advisor = 'Aisha';
+	$sts -> save();
+});
+
+Route::get('/update1', function(){
+	$sts = Student::find(19030303);
+	$sts -> ID = '19030303';
+	$sts -> Name = 'John';
+	$sts -> Date_of_birth = '2001-09-09';
+	$sts -> GPA = '2.1';
+	$sts -> Advisor = 'Aisha';
+	$sts -> save();
+});
+
+Route::get('/delete1', function(){
+	Student::destroy(19030303);
+});
 
 
